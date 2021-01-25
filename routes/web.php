80245cc850402cc28e550155d57a13f604571633
yezid00 <olanaseyezid@gmail.com>
2021-01-25 15:50:13 +0100

    set up routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('add/image',[ImageController::class,'add'])->name('add');
+Route::post('save/image',[ImageController::class,'saveImage'])->name('save');
+Route::delete('image/{image_id}',[ImageController::class,'deleteImage'])->name('delete');
+Route::delete('delete/selected',[ImageController::class,'deleteAll'])->name('delete.all');
